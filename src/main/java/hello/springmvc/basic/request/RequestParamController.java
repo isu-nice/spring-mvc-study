f4a5f0e2ddf541c99d429c6b0cdb5a214a0f5ea2
isu-nice <isu010614@naver.com>
2023-11-09 15:58:32 +0900

    add: 요청 파라미터 학습

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -34,5 +34,16 @@ public class RequestParamController {
         return "ok";
     }
 
+    // 파라미터 명 일치시켜주면 괄호 안 변수명 생략 가능
+    @ResponseBody
+    @RequestMapping("/request-param-V3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age
+    ) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
 
 }
